40b97388c9b9db4e806439ee3e84872b1d7724eb
Om Singh <om.singh@siemens.com>
2015-06-08 10:25:10 +0200

    first

---
+++ src/java8/Test.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) Siemens AG 2015 ALL RIGHTS RESERVED.
+ *
+ * R8  
+ * 
+ */
+
+package java8;
+
+public class Test
+{
+    
+}
+
+
+/*
+ * Copyright (c) Siemens AG 2015 ALL RIGHTS RESERVED
+ *
+ * R8
+ */
